8fc7d8f14b1a02a6055f2468342d11b4cd25f798
hiromia006 <hiromia006@gmail.com>
2026-03-10 23:01:35 +0600

    feat: add video recording and screenshot functionality to BaseTest

--- src/test/java/com/emi/calculator/test/BaseTest.java
+++ src/test/java/com/emi/calculator/test/BaseTest.java
@@ -3,13 +3,23 @@ package com.emi.calculator.test;
 import com.emi.calculator.pages.BasePage;
 import com.emi.calculator.pages.Page;
 import io.appium.java_client.android.AndroidDriver;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 public class BaseTest {
     public AndroidDriver driver;
@@ -28,6 +38,7 @@ public class BaseTest {
 
 
             driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), caps);
+            driver.startRecordingScreen();
             pg = new BasePage(driver);
 
         } catch (MalformedURLException e) {
@@ -39,10 +50,40 @@ public class BaseTest {
     public void tearDownMobileApp() {
         System.out.println("Tearing down mobile app testing environment...");
         // Code to clean up mobile app testing environment goes here
+        recordingVideo();
         driver.quit();
     }
 
     public WebDriver getWebDriver() {
         return driver;
+    }
+
+    public void takeScreenshot() {
+        try {
+            File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            String currentDir = System.getProperty("user.dir") + "/build/screenshots/";
+            FileUtils.copyFile(scrFile, new File(currentDir + System.currentTimeMillis() + ".png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void recordingVideo() {
+        String base64String = driver.stopRecordingScreen();
+        byte[] data = Base64.decodeBase64(base64String);
+        String destinationPath = System.getProperty("user.dir") + "/build/videos/";
+        File theDir = new File(destinationPath);
+        if (!theDir.exists()) {
+            theDir.mkdirs();
+        }
+
+        String filePath = destinationPath + "/" + driver.getDeviceTime().replace(":", "_").replace("+", " ") + ".mp4";
+        System.out.println("filePath : " + filePath);
+        Path path = Paths.get(filePath);
+        try {
+            Files.write(path, data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
